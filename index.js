d84ea53e8a3f632bd629bbe0ce72dab827915692
yonasketema <yonask.kitaw@gmail.com>
2023-07-04 17:41:58 +0300

    connect to database

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const userRouter = require("./routes/userRoutes");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/nodeauth", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("> DB connection successful ... ");
+  });
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`> App running on port ${PORT}...`);
+  console.log(`> App running on port ${PORT} ...`);
 });
