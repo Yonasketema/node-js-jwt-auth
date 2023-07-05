80c2d482f4f0fd8513c2d8afec1bba9278a711fb
yonasketema <yonask.kitaw@gmail.com>
2023-07-05 20:15:27 +0300

    add signup token

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,9 +1,25 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET_KEY, {
+    expiresIn: process.env.EXPIRE_IN,
+  });
+};
+
 exports.signup = async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
+
+  const token = signToken(newUser.id);
 
   newUser.password = undefined;
 
-  res.status(201).json(newUser);
+  res.status(201).json({ token, user: newUser });
 };
